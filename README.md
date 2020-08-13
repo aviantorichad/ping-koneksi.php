9bcf486190f12e30e52bfd1a2089431192558324
Richad Avianto <aviantorich@gmail.com>
2020-08-13 11:12:51 +0700

    add detail, change layout and add timer changer

--- README.md
+++ README.md
@@ -1,21 +1,31 @@
 # ping-koneksi.php
+
 ![screenshot](https://github.com/aviantorichad/ping-koneksi.php/blob/master/ss.png)
 
 ## INSTALLATION
-+ clone or download this repo
-+ [NATIVE PHP]
-```cd ping-koneksi.php```
-then
-```php -S 0.0.0.0:9090 ```
 
-+ [XAMPP] extract/copy paste to your htdocs folder 
+- clone or download this repo
+- [NATIVE PHP]
+  `cd ping-koneksi.php`
+  then
+  `php -S 0.0.0.0:9090`
 
-+ access it from your browser (native)
-``` localhost:9090/index.php```
-or (xampp)
-``` localhost/ping-koneksi.php/index.php```
+- [XAMPP] extract/copy paste to your htdocs folder
+
+- access it from your browser (native)
+  `localhost:9090/index.php`
+  or (xampp)
+  `localhost/ping-koneksi.php/index.php`
 
 ## USING IT
-+ Play Ping : for interval ping (10s default)
-+ Timer : timer
-+ Ping Sekali : ping once
+
+- Play Ping : for interval ping (10s default)
+- Timer : timer (click this button to change timer)
+- Ping Sekali : ping once
+
+## LEGEND
+
+- green : 200 <= (ping) [ok]
+- yellow : 200 > (ping) < 500 [warning]
+- red : (ping) > 500 [danger]
+- blue : down (not connected) [RIP]
--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
-$start = 10;
+$get_timer = isset($_GET['timer']) ? $_GET['timer'] : 10;
 
+$start = $get_timer;
 // ************* list ip/host.begin **************
 $hosts = [];
 $hosts['AP'] = '192.168.1.1';
@@ -14,15 +15,14 @@ $hosts['INSTAGRAM'] = 'instagram.com';
 $hosts['TWITTER'] = 'twitter.com';
 $hosts['DETIK'] = 'detik.com';
 // ************* list ip/host.end ****************
-
-
 function Ping($host, $timeout = 10)
 {
+  $timeout = $get_timer ?? $timeout;
   if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-    // echo 'njenengan ngagem windows';
+    // echo 'This is a server using Windows!';
     $os = "win";
   } else {
-    // echo 'njenengan mboten ngagem windows';
+    // echo 'This is a server not using Windows!';
     $os = "lin";
   }
 
@@ -41,13 +41,13 @@ function Ping($host, $timeout = 10)
     foreach ($output as $cline) {
       if ($os == "lin") {
         if (strpos($cline, ' bytes from ') !== false) {
-          $out = (int) ceil(floatval(substr($cline, strpos($cline, 'time=') + 5)));
+          $out = (int)ceil(floatval(substr($cline, strpos($cline, 'time=') + 5)));
           return $out;
         }
       }
       if ($os == "win") {
         if (strpos($cline, 'Reply from ') !== false) {
-          $out = (int) ceil(floatval(substr($cline, strpos($cline, 'time=') + 5)));
+          $out = (int)ceil(floatval(substr($cline, strpos($cline, 'time=') + 5)));
           return $out;
         }
       }
@@ -66,12 +66,15 @@ if (isset($_GET['host'])) {
 
 <script>
   function ngeping(liid, host) {
+    window['total-' + liid]++;
     var lid = document.getElementById(liid);
     var hid = document.getElementById("h" + liid);
 
     xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
+        //total
+        document.getElementById('total-' + liid).innerHTML = window['total-' + liid];
         var result = this.responseText;
         var width = this.responseText;
         var cssHeight = this.responseText * 50 / 250;
@@ -83,13 +86,28 @@ if (isset($_GET['host'])) {
         var currentdate = new Date();
         var datetime = currentdate.getDate() + "/" +
           (currentdate.getMonth() + 1) + "/" +
-          currentdate.getFullYear() + " @ " +
+          currentdate.getFullYear() + " " +
           currentdate.getHours() + ":" +
           currentdate.getMinutes() + ":" +
           currentdate.getSeconds();
 
         lid.style.width = width + "px";
         lid.innerHTML = result;
+
+        //min
+        if (Number(window['min-' + liid]) == 0) {
+          window['min-' + liid] = result;
+          document.getElementById('min-' + liid).innerHTML = result;
+        } else if (Number(result) < Number(window['min-' + liid])) {
+          window['min-' + liid] = result;
+          document.getElementById('min-' + liid).innerHTML = result;
+        } else {}
+
+        //max
+        if (Number(result) > Number(window['max-' + liid])) {
+          window['max-' + liid] = result;
+          document.getElementById('max-' + liid).innerHTML = result;
+        }
 
         if (result >= 500) {
           lid.style.backgroundColor = "#f77";
@@ -100,10 +118,12 @@ if (isset($_GET['host'])) {
           para.style.display = "inline-block";
           para.style.verticalAlign = "bottom";
           para.style.height = cssHeight;
-          para.style.maxHeight = "100px";
+          para.style.maxHeight = "90px";
           para.title = datetime;
           var node = document.createTextNode(result);
           para.insertBefore(node, para.firstChild);
+          //last up
+          document.getElementById('lu-' + liid).innerHTML = ' #' + window['total-' + liid] + ' @ ' + datetime;
         } else if (result > 200 && result < 500) {
           lid.style.backgroundColor = "#ff7";
           var para = document.createElement("span");
@@ -114,14 +134,16 @@ if (isset($_GET['host'])) {
           para.style.verticalAlign = "bottom";
           para.style.width = "20px";
           para.style.height = cssHeight;
-          para.style.maxHeight = "100px";
+          para.style.maxHeight = "80px";
           para.title = datetime;
           var node = document.createTextNode(result);
+          //last up
+          document.getElementById('lu-' + liid).innerHTML = ' #' + window['total-' + liid] + ' @ ' + datetime;
           para.insertBefore(node, para.firstChild);
         } else if (result == '-') {
-          lid.style.backgroundColor = "#ddd";
+          lid.style.backgroundColor = "white";
           var para = document.createElement("span");
-          para.style.backgroundColor = "#ddd";
+          para.style.backgroundColor = "blue";
           para.style.padding = "3px";
           para.style.color = "#222";
           para.style.display = "inline-block";
@@ -132,6 +154,10 @@ if (isset($_GET['host'])) {
           para.title = datetime;
           var node = document.createTextNode(result);
           para.insertBefore(node, para.firstChild);
+          //down nih
+          window['down-' + liid]++;
+          document.getElementById('down-' + liid).innerHTML = window['down-' + liid];
+          document.getElementById('ld-' + liid).innerHTML = ' #' + window['total-' + liid] + ' @ ' + datetime;
         } else {
           lid.style.backgroundColor = "#7f7";
           var para = document.createElement("span");
@@ -142,15 +168,24 @@ if (isset($_GET['host'])) {
           para.style.verticalAlign = "bottom";
           para.style.width = "20px";
           para.style.height = cssHeight;
-          para.style.maxHeight = "100px";
+          para.style.maxHeight = "70px";
           para.title = datetime;
           var node = document.createTextNode(result);
           para.insertBefore(node, para.firstChild);
+          //last up
+          document.getElementById('lu-' + liid).innerHTML = ' #' + window['total-' + liid] + ' @ ' + datetime;
         }
         hid.insertBefore(para, hid.firstChild);
       }
+      window['persen-' + liid] = (Number(window['total-' + liid]) - Number(window['down-' + liid])) / Number(window['total-' + liid]) * 100;
+      document.getElementById('persen-' + liid).innerHTML = window['persen-' + liid].toFixed(2) + '%';
+      if (window['persen-' + liid] < 50) {
+        document.getElementById('th-' + liid).style.backgroundColor = 'maroon';
+      } else {
+        document.getElementById('th-' + liid).style.backgroundColor = 'transparent';
+      }
     };
-    xhttp.open("GET", "?host=" + host, true);
+    xhttp.open("POST", "?host=" + host, true);
     xhttp.send();
   }
 
@@ -194,8 +229,15 @@ if (isset($_GET['host'])) {
   document.addEventListener("DOMContentLoaded", function(event) {
     //code autoload...
   });
-</script>
 
+  function gantiTimer() {
+    var a = prompt('Timer (default:10)', 10);
+    if (a) {
+      window.location.href = '?timer=' + a;
+    }
+  }
+</script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Pingku by @aviantorichad</title>
 <style>
   body {
@@ -223,18 +265,25 @@ if (isset($_GET['host'])) {
     width: 150px;
     display: inline-block;
     font-weight: bold;
+    color: gold;
   }
 
-  .desc {
+  .desc,
+  .status,
+  .total {
     width: 150px;
     font-size: 10px;
     display: inline-block;
   }
 
   .result {
-    padding: 3px;
     display: inline-block;
     color: #000;
+    font-size: 10px;
+    white-space: nowrap;
+    width: auto;
+    width: 300px;
+    overflow: scroll;
   }
 
   .hist .result {
@@ -245,7 +294,7 @@ if (isset($_GET['host'])) {
   }
 
   ::-webkit-scrollbar {
-    width: 10px;
+    width: 0px;
   }
 
   /* Track */
@@ -274,10 +323,37 @@ if (isset($_GET['host'])) {
     background: #034;
     color: #fff;
   }
+
+  .tr {
+    width: 33.33%;
+    display: inline-block;
+    margin-bottom: 20px;
+  }
+
+  .th {
+    vertical-align: bottom;
+    display: inline-block;
+  }
+
+  .td {
+    height: 100px;
+    vertical-align: bottom;
+    background: #111;
+    display: inline-block;
+  }
+
+  @media only screen and (max-width: 600px) {
+    .tr {
+      width: 100%;
+      display: inline-block;
+      margin-bottom: 20px;
+    }
+
+  }
 </style>
 <button name="btnDoingInterval" id="btnDoPingInterval" onclick="doPingInterval()">[>] Play Ping</button>
 <button name="btnStopPingInterval" id="btnStopPingInterval" onclick="stopPingInterval()" style="display:none;">[x] Stop Ping</button>
-<button name="counter">Timer: <span id="counter">0</span></button>
+<button name="counter" onClick="gantiTimer()">Timer: <span id="counter">0</span></button>
 <button name="btnDoPing" id="btnDoPing" onclick="doPing()">Ping Sekali</button>
 <ul>
   <?php foreach ($hosts as $key => $value) {
@@ -285,38 +361,45 @@ if (isset($_GET['host'])) {
   }
   ?>
 </ul>
-<table style="width:100%" border=0>
-  <?php foreach ($hosts as $key => $value) {
-    echo '<tr>
-            <td style="width:100px;vertical-align:bottom;height:100px;">
+<div class="table" style="width:100%" border=0>
+  <?php
+  $x = 0;
+  foreach ($hosts as $key => $value) {
+    $bgcolor = ($x % 2 == 0) ? '#000' : '#111';
+    echo '<div class="tr">
+            <div class="th" id="th-' . $key . '">
               <span class="label">' . $key . '</span>
               <br/>
-              <span class="desc">(' . $value . ')</span>
-            </td>
-            <td style="height:100px;vertical-align:bottom;background:#111">
-              <div id="h' . $key . '" title="' . $value . '" class="result custom_scrollbar" style="font-size:10px;white-space: nowrap;width:1000px;overflow:auto;"></div>
-            </td>
-          </tr>';
+              <span class="desc"><a href="http://' . $value . '" target="_blank" style="color:#da0">(' . $value . ')</a></span>
+              <br/>
+              <span class="status">min/max: <b id="min-' . $key . '">0</b>/<b id="max-' . $key . '">0</b></span>
+              <br/>
+              <span class="total">down/total: <b id="down-' . $key . '">0</b>/<b id="total-' . $key . '">0</b></span>
+              <br/>
+              <span class="total" title="last up">lU <u id="lu-' . $key . '">-</u></span>
+              <br/>
+              <span class="total" title="last down">lD <u id="ld-' . $key . '">-</u></span>
+              <br/>
+              <span class="persen"><b id="persen-' . $key . '">0%</b></span>
+            </div>
+            <div class="td" style="background:' . $bgcolor . '">
+              <div id="h' . $key . '" title="' . $value . '" class="result custom_scrollbar"></div>
+              <div style="height:100px;background:red;display:inline-block;"></div>
+              
+          <script>
+          window["persen-' . $key . '"] = 0;
+          window["total-' . $key . '"] = 0;
+          window["down-' . $key . '"] = 0;
+          window["lu-' . $key . '"] = 0;
+          window["ld-' . $key . '"] = 0;
+          window["min-' . $key . '"] = 0;
+          window["old-min-' . $key . '"] = 0;
+          window["max-' . $key . '"] = 0;
+          window["old-max-' . $key . '"] = 0;
+          </script>
+            </div>
+          </div>';
+    $x++;
   }
   ?>
-</table>
-<!-- 
-kula nuwun
-matur nuwun sampun ngagem aplikasi niki
-aplikasi niki awalipun damel monitor koneksi internal kulo
-sakniki badhe kulo share damel njenengan2 menawi gadah kebutuhan ingkang sami kaliyan kulo
-nuwun sewu menawi badhe dimodifikasi ampun nghapus/ngubah "kredit" nami kulo ting aplikasi niki, 
-sakestu, sepele nanging berarti kagem kulo
-
-menawi badhe donasi kulo nggih nampi lan saged dikirim ting bank meniko
-BANK BCA : 8715-9843-81 a/n Richad Avianto
-PAYPAL : aviantorich@gmail.com
-
-
-*dilarang keras memodifikasi/menghapus kredit aviantorichad dan mengkomersilkan aplikasi ini tanpa seijin dari aviantorich@gmail.com
-
-author: Richad Avianto
-alias: aviantorichad
-github: github.com/aviantorichad 
-kritik/saran/bug: kirim email ke aviantorich@gmail.com atau kirim issue ke github
--->
+</div>
